d3096572fee977570e5b5e4a310f51bf2c48a2ea
radkye <rzwolski22@gmail.com>
2015-12-10 14:36:41 +0100

    Added lettersConverter class

--- test/lettersConverterTest.cpp
+++ test/lettersConverterTest.cpp
@@ -6,6 +6,7 @@ TEST (lettersConverter, FirstLetterShouldBeUpperCase) {
   lettersConverter converter;
   std::string word("ala");
   std::string desiredWord("Ala");
+  
   auto resultWord = converter.firstLetterToUpperCase(word);
   
   EXPECT_TRUE (resultWord == desiredWord);
@@ -15,6 +16,7 @@ TEST (lettersConverter, EveryLetterShouldBeUpperCase) {
   lettersConverter converter;
   std::string word("ala");
   std::string desiredWord("ALA");
+  
   auto resultWord = converter.everyLetterToUpperCase(word);
   
   EXPECT_TRUE (resultWord == desiredWord);
@@ -24,6 +26,7 @@ TEST (lettersConverter, EveryLetterShouldBeLowerCase) {
   lettersConverter converter;
   std::string word("ala");
   std::string desiredWord("ala");
+  
   auto resultWord = converter.everyLetterToLowerCase(word);
   
   EXPECT_TRUE (resultWord == desiredWord);
