77b3ec751eccde8f5fad820a911de8c3e716de94
Klapeyron <t.m@o2.pl>
2015-12-11 22:17:49 +0100

    Added references

--- include/lettersConverter.hpp
+++ include/lettersConverter.hpp
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <iterator>
 
 namespace lettersConverter {
-  std::string onlyFirstLetterUpperCase(std::string);
-  std::string everyLetterToUpperCase(std::string);
-  std::string everyLetterToLowerCase(std::string);
+  std::string onlyFirstLetterUpperCase(std::string &);
+  std::string everyLetterToUpperCase(std::string &);
+  std::string everyLetterToLowerCase(std::string &);
 };
--- src/lettersConverter.cpp
+++ src/lettersConverter.cpp
@@ -1,17 +1,17 @@
 #include "lettersConverter.hpp"
 
-std::string lettersConverter::onlyFirstLetterUpperCase(std::string word) {
+std::string lettersConverter::onlyFirstLetterUpperCase(std::string & word) {
   auto lowerCaseWord = everyLetterToLowerCase(word);
   std::transform(lowerCaseWord.begin(), lowerCaseWord.begin()+1, lowerCaseWord.begin(), ::toupper);
   return lowerCaseWord;
 }
 
-std::string lettersConverter::everyLetterToUpperCase(std::string word) {
+std::string lettersConverter::everyLetterToUpperCase(std::string & word) {
   std::transform(word.begin(), word.end(), word.begin(), ::toupper);
   return word;
 }
 
-std::string lettersConverter::everyLetterToLowerCase(std::string word) {
+std::string lettersConverter::everyLetterToLowerCase(std::string & word) {
   std::transform(word.begin(), word.end(), word.begin(), ::tolower);
   return word;
 }
